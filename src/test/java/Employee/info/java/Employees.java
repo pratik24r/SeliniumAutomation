cb4b43b297d7bd5c549a525ac3f3d31cf14b1665
pratik <pratik24r@gmail.com>
2024-01-16 22:58:04 +0530

    updated

--- src/test/java/Employee/info/java/Employees.java
+++ src/test/java/Employee/info/java/Employees.java
@@ -2,15 +2,16 @@ package Employee.info.java;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Employees {
 	
-	@BeforeTest
+	@Test
 	public void launch() {
 		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.flipkart.com/");
 	
