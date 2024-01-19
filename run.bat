8f26e612d320c72be00eb56d88a59bdc4329d8cd
pratik <pratik24r@gmail.com>
2024-01-19 16:33:19 +0530

    updated

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\FS-LP-07\eclipse-workspace\RegistrationFrom
+mvn clean install
--- src/test/java/Employee/info/java/Employees.java
+++ src/test/java/Employee/info/java/Employees.java
@@ -2,33 +2,38 @@ package Employee.info.java;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Employees {
-	
+	WebDriver driver;
 	@Test
 	public void launch() {
+	//	System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chrome-linux64\\chrome-linux64\\chromrdriver.exe");)
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+	    driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
+		driver.get("http://localhost:8181/azure-employee-2.2-SNAPSHOT/");
 	
 	}
 	
 	@Test
-	public void test1() {
-		System.out.println("this is testcase1");
-	}
-	
-	@Test
-	public void test2() {
-		System.out.println("this is testcase2");
-	}
-	
-	@Test
-	public void test3() {
-		System.out.println("this is testcase3");
+	public void user_name() {
+
+	    driver.findElement(By.id("user_input")).sendKeys("pratik rathi");
+
+	    driver.findElement(By.id("add_input")).sendKeys("pune");
+
+	    driver.findElement(By.id("age_input")).sendKeys("25");
+
+	    driver.findElement(By.id("quli_input")).sendKeys("BCCA");
+
+	    driver.findElement(By.id("jd_input")).sendKeys("27");
+
+	    driver.findElement(By.xpath("/html/body/div[2]/input")).click();
+
 	}
 }
